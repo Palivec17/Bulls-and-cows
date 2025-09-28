caa495d1290fda7558da77b5d1796621178370da
Palivec17 <palivec17@seznam.cz>
2025-09-28 21:42:51 +0200

    Vytvořena funkce na validaci hráčova tipu

--- main.py
+++ main.py
@@ -10,3 +10,17 @@ def vygeneruj_tajne_cislo() -> str:
     zbyvajici_cislice = [c for c in "0123456789" if c != prvni_cislice]
     random.shuffle(zbyvajici_cislice)
     return prvni_cislice + ''.join(zbyvajici_cislice[:DELKA_CISLA - 1])
+
+
+def validuj_tip(tip: str) -> str | None:
+    """Zkontroluje platnost hráčova tipu."""
+    if len(tip) != DELKA_CISLA:
+        return f"Tvůj tip musí mít přesně {DELKA_CISLA} číslice."
+    if not tip.isdigit():
+        return "Tip smí obsahovat pouze číslice."
+    if tip[0] == '0':
+        return "Číslo nesmí začínat nulou."
+    if len(set(tip)) != DELKA_CISLA:
+        return "Číslice se nesmí opakovat. Zadej unikátní číslice."
+    return None
+
